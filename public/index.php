b69ed4201f62eae77403c54d4f0ab9f0fefacfce
Giovanni Fushimi <giiovanni.kf@gmail.com>
2022-08-16 00:14:53 -0300

    Updates on development environment

--- public/index.php
+++ public/index.php
@@ -4,10 +4,13 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use PomoManager\Controller\controllersInterface;
 
-$caminho = $_SERVER['PATH_INFO'];
+$caminho = $_SERVER['REQUEST_URI'];
 $rotas = require __DIR__ . '/../config/routes.php';
 
 if (!array_key_exists($caminho, $rotas)) {
+    echo "<pre>";
+    print_r($GLOBALS);
+    echo "</pre>";
     http_response_code(404);
     exit();
 }
