a46c5a6ba2d26fb35e5b90db0397b4d7704f2f9b
Giovanni Fushimi <giiovanni.kf@gmail.com>
2022-08-16 10:57:58 -0300

    Revert "Updates on development environment"
    This reverts commit b69ed4201f62eae77403c54d4f0ab9f0fefacfce.

--- public/index.php
+++ public/index.php
@@ -4,13 +4,10 @@ require __DIR__ . '/../vendor/autoload.php';
 
 use PomoManager\Controller\controllersInterface;
 
-$caminho = $_SERVER['REQUEST_URI'];
+$caminho = $_SERVER['PATH_INFO'];
 $rotas = require __DIR__ . '/../config/routes.php';
 
 if (!array_key_exists($caminho, $rotas)) {
-    echo "<pre>";
-    print_r($GLOBALS);
-    echo "</pre>";
     http_response_code(404);
     exit();
 }
